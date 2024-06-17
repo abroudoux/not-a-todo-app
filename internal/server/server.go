941f148f017aa9efa00eda86610d2f2925bb7007
arthur <arthur.broudoux@gmail.com>
2024-06-17 23:38:15 +0200

    feat: create server

---
+++ internal/server/server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos []Todo = []Todo{
+	 {ID: 1, Title: "Go to the bank", Completed: false},
+	{ID: 2, Title: "Buy some bread", Completed: true},
+	{ID: 3, Title: "Learn Go", Completed: false},
+}
+
+func getTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
+func NewServer(port string) {
+	r := gin.Default()
+
+	r.GET("/todos", getTodos)
+
+	r.Run(":" + port)
+}
